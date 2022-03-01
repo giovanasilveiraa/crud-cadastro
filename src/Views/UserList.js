e1ce0337788ac92c7a580794c15f43042f82bd5d
giovanasilveiraa <giovana.silveira01@etec.sp.gov.br>
2022-03-01 19:14:42 -0300

    user

--- src/Views/UserList.js
+++ src/Views/UserList.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { View, Text, FlatList } from 'react-native';
+import { ListItem, Avatar } from 'react-native-elements';
 import users from '../data/users'
-import { ListItem } from 'react-native-elements'
 
 export default props => {
 
     function getUserItem({ item: user }) {
         return (
-           <ListItem
+            <ListItem 
             leftAvatar={{source: {uri: user.avatarUrl}}}
             key={user.id}
             title={user.name}
-           
-           />
-
+            subtitle={user.email}
+            bottomDivider
+        
+        />
         )
+        
     }
 
     return (
